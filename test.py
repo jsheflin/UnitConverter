25b5452d115b3179f342ecc3a27512971d2ec566
Sheflin, John Robert <jrsheflin@gmail.com>
2020-05-04 13:29:12 -0600

    have to write test

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+
+from start import convert
+
+class TestConvert(unittest.TestCase):
+
+# pseudo code:
+# test input validation
+# loop through list of volumes, convert, repeat with temps
+    
+
+
+
