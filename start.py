c85e2d12e452372cad0027bc953a29a94eb7aa1c
Sheflin, John Robert <jrsheflin@gmail.com>
2020-05-04 20:28:26 -0600

    change structure of start to get test to work

--- start.py
+++ start.py
@@ -37,8 +37,39 @@ MULTIPLIERS_FROM_STD = {
 }
 
 
-def get_user_input(choice):
-    units = ', '.join(MULTIPLIERS_TO_STD[choice].keys())
+def convert(source_unit, source_val, convert_to,answer):
+    temp = ', '.join(MULTIPLIERS_TO_STD['temp'].keys())
+    volume = ', '.join(MULTIPLIERS_TO_STD['volume'].keys())
+
+    if source_unit in volume:
+        # source_unit, source_val, convert_to,answer = get_user_input('volume')
+        conversion = round(source_val * \
+                               MULTIPLIERS_TO_STD['volume'][source_unit] * \
+                               MULTIPLIERS_FROM_STD['volume'][convert_to], 1)
+        are_close = math.isclose(answer, conversion)          
+    else:
+      # source_unit, source_val, convert_to,answer = get_user_input('temp')
+      k = MULTIPLIERS_TO_STD['temp'][source_unit](float(source_val))
+      if convert_to == 'C':
+        conversion = k - 273.15
+      elif convert_to == 'F':
+        conversion = k * 1.8 - 459.67
+      elif convert_to == 'R':
+        conversion = k * 1.8
+      else: conversion = k
+      are_close = math.isclose(answer, conversion)    
+    if are_close:
+          print('correct')
+    elif answer != conversion: 
+          print ('incorrect')        
+    
+    return conversion
+
+def main():
+
+    tunits = ', '.join(MULTIPLIERS_TO_STD['temp'].keys())
+    vunits = ', '.join(MULTIPLIERS_TO_STD['volume'].keys())
+    units = tunits + ' , ' + vunits
     while True:
        try:
          source_unit = input('\nEnter source unit (%s): ' % units)
@@ -54,73 +85,38 @@ def get_user_input(choice):
       source_val = float(input('How many %s? ' % source_unit))
     except ValueError:
       print ('invalid')
-    while True:
+    
+    if (source_unit in vunits):
+      while True:
+        try:
+          convert_to = input('Convert to? (%s): ' % vunits)
+        except ValueError:
+          print ('invalid')
+          continue
+        if convert_to not in vunits:
+          print("Sorry, I didn't understand that.")
+          continue
+        else:
+              break
+    else:
+      while True:
        try:
-         convert_to = input('Convert to? (%s): ' % units)
+         convert_to = input('Convert to? (%s): ' % tunits)
        except ValueError:
          print ('invalid')
          continue
-       if convert_to not in units:
+       if convert_to not in tunits:
          print("Sorry, I didn't understand that.")
          continue
        else:
             break  
+
     try:
       answer = float(input('Answer?'))
     except ValueError:
       print ('incorrect')
-    return source_unit, source_val, convert_to, answer
-
-def get_correct_choice(prompt):
-
-    print ("""Unit Converter
-    1. Volume
-    2. Temperature""")
     
-    while True:
-        try:
-            value = int(input(prompt))
-        except ValueError:
-            print("Sorry, I didn't understand that.")
-            continue
-
-        if value < 1 or value > 2:
-            print("Sorry, you must choose 1 or 2.")
-            continue
-        else:
-            break
-    return value
-
-def convert(choice):
-    if choice == 1:
-        source_unit, source_val, convert_to,answer = get_user_input('volume')
-        conversion = round(source_val * \
-                               MULTIPLIERS_TO_STD['volume'][source_unit] * \
-                               MULTIPLIERS_FROM_STD['volume'][convert_to], 1)
-        are_close = math.isclose(answer, conversion)          
-    elif choice == 2:
-      source_unit, source_val, convert_to,answer = get_user_input('temp')
-      k = MULTIPLIERS_TO_STD['temp'][source_unit](float(source_val))
-      if convert_to == 'C':
-        conversion = k - 273.15
-      elif convert_to == 'F':
-        conversion = k * 1.8 - 459.67
-      elif convert_to == 'R':
-        conversion = k * 1.8
-      else: conversion = k
-      are_close = math.isclose(answer, conversion)    
-    if are_close:
-          print('correct')
-    elif answer != conversion: 
-          print ('incorrect')        
-    print(conversion)      
-
-def main():
-
-    choice = get_correct_choice('What do you want to convert: ')
-    response = convert(choice)
-    
-    return response 
+    print (convert(source_unit, source_val, convert_to,answer))
    
        
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 import unittest
-from unittest.mock import patch
+import start
 
-from start import convert
+class TestFactorial(unittest.TestCase):
 
-class TestConvert(unittest.TestCase):
+    def test_c_to_tb(self):
+        source_unit = 'c'
+        source_val = 11.1
+        convert_to = 'tb'
+        answer = 222.2
+       
+        res = start.convert(source_unit, source_val, convert_to,answer)
+        self.assertEqual(res, 177.2)
 
-# pseudo code:
-# test input validation
-# loop through list of volumes, convert, repeat with temps
-    
+# for i in [0, 255.37, 273.15, 373.15, 473.15]:
+#         for unit in start.MULTIPLIERS_FROM_STD.values():
+#             print("%.2f K is %.2f %s" % (i, unit.from_kelvins(i), unit.name))
+#         print()
 
 
-
+if __name__ == '__main__':
+    unittest.main()
